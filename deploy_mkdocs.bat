b999d1498249faae05ccebb9254b7e9c4f496978
yogiksuryatmika19 <yogiksuryatmika@gmail.com>
2026-04-04 09:57:50 +0800

    generate otomatis message

--- deploy_mkdocs.bat
+++ deploy_mkdocs.bat
@@ -1,8 +1,9 @@
 @echo off
-REM Script otomatis untuk add, commit, dan deploy MkDocs
+REM Script otomatis untuk add, commit, build, dan deploy MkDocs
 
 git add .
 git commit -m "generate otomatis message"
+mkdocs build
 mkdocs gh-deploy --force
 
 echo.
